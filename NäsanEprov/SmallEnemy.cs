c3a11baaf78d2c0bfb8f5d57ab899b4f079a0f73
ErikEkelof <60339719+ErikEkelof@users.noreply.github.com>
2021-05-24 15:09:21 +0200

    l

--- NäsanEprov/SmallEnemy.cs
+++ NäsanEprov/SmallEnemy.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace NäsanEprov
 {
@@ -6,7 +7,8 @@ namespace NäsanEprov
     {
         public int hp = 1; 
 
-
-        
+        public override void x (){
+            
+        }
     }
 }
